a3511134186482711306a5b4edd42adfa251f2d9
tanishk15sharma <tanishk1582002@gmail.com>
2021-11-22 19:18:21 +0530

    intermediate commit
    I have made the functioning of the app proper and added the styling also .

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const inputExpenseName = document.querySelector("#inputExpense");
 const inputExpenseAmount = document.querySelector("#expenseAmount");
 const addButton = document.querySelector("#addbtn");
-const displayExpenseList = document.querySelector("#expenseContainer");
+const displayExpenseList = document.querySelector("#expenseListContainer");
 const expenseTotal = document.querySelector("#expenseTotal");
 
-addButton.addEventListener("click",totalExpenseFinder);
+
+addButton.addEventListener("click", totalExpenseFinder);
+addButton.addEventListener("keyup", function(event) {
+    if(event.keyCode === 13  ){
+        event.preventDefault();
+        addButton.click()
+    }
+} );
+
 
 let expenseCounter = 0;
 let allExpenseList = [];
@@ -21,21 +29,70 @@ function totalExpenseFinder() {
 
     expenseCounter = expenseCounter + amount;
     // console.log(expenseCounter); 
-    expenseTotal.innerHTML = expenseCounter;
+    expenseTotal.innerHTML = "Total: " + expenseCounter + " ₹";
+
+    inputExpenseAmount.value  = "";
+    inputExpenseName.value = "";
 
     //------------------------------------------//
-   //showing the list of expenses by the user//
+    //showing the list of expenses by the user//
     expenseListItem.name = expenseName;
     expenseListItem.amount = expenseAmount;
+    expenseListItem.moment = new Date();
 
     //console.log(expenseListItem);
     allExpenseList.push(expenseListItem);
     // console.log(allExpenseList);
 
-    let outputExpenseList = allExpenseList.map(expense => {
-        return `<div> ${expense.name} = ${expense.amount}</div>  `
-    }).join("");
+    renderListArry(allExpenseList);
 
-    //console.log(output);
-    displayExpenseList.innerHTML = outputExpenseList;
+    // inputExpenseAmount.innerText = "";
+    // inputExpenseName.innerText = "";
 };
+
+function getDateString(datemoment) {
+    return datemoment.toLocaleString("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+    })
+};
+
+function showUndeleteItems(datevalue) {
+    let arr = [];
+    for (let i = 0; i < allExpenseList.length; i++) {
+        if (allExpenseList[i].moment.valueOf() !== datevalue) {
+            arr.push(allExpenseList[i]);
+        }
+    }
+    allExpenseList = arr;
+   renderListArry(allExpenseList);
+};
+
+function renderListArry(listOfArrys) {
+
+    let outputExpenseList = listOfArrys.map(expense => createListItem(expense)).join("");
+    displayExpenseList.innerHTML = outputExpenseList;
+
+};
+
+function createListItem({
+    name,
+    amount,
+    moment
+}) {
+    return ` <li class="list-group-item d-flex justify-content-between">
+    <div class="d-flex flex-column">
+    ${name}
+        <small class="text-muted">${getDateString(moment)} </small>
+    </div>
+    <div>
+        <span class="px-5"> 
+        ${amount}
+        </span>
+        <button type="button" class="btn btn-outline-danger btn-sm"  onclick=" showUndeleteItems(${moment.valueOf()})">
+            <i class="fas fa-trash-alt"></i>
+        </button>
+    </div>
+</li>`
+}
